53f6db4a733ac882e465010aa1a619b77170e60e
Graeme Harker <graeme.harker@gmail.com>
2024-12-14 21:23:42 +0000

    add traits

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,67 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::convert::From;
+use std::convert::Into;
+use std::ops::Add;
+use std::cmp::PartialEq;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self { value: value.into() }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        Self { value: (*value).into() }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        Self { value: (*value).into() }
+    }
+}
+
+impl Add for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: Self::Output) -> Self::Output {
+        SaturatingU16 { value: self.value.saturating_add(rhs.value) }
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        SaturatingU16 { value: self.value.saturating_add(rhs) }
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        SaturatingU16 { value: self.value.saturating_add(rhs.value) }
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
--- exercises/05_ticket_v2/09_error_trait/src/lib.rs
+++ exercises/05_ticket_v2/09_error_trait/src/lib.rs
@@ -3,17 +3,39 @@
 //  The docs for the `std::fmt` module are a good place to start and look for examples:
 //  https://doc.rust-lang.org/std/fmt/index.html#write
 
+use std::fmt::Display;
+use std::error::Error;
+
+#[derive(Debug)]
 enum TicketNewError {
     TitleError(String),
     DescriptionError(String),
 }
+
+impl Display for TicketNewError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TicketNewError::TitleError(message) => 
+                write!(f, "{}", message),
+            TicketNewError::DescriptionError(message) => 
+                write!(f, "{}", message),
+        }
+    }
+}
+
+impl Error for TicketNewError {}
 
 // TODO: `easy_ticket` should panic when the title is invalid, using the error message
 //   stored inside the relevant variant of the `TicketNewError` enum.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description.clone(), status.clone()) {
+        Err(TicketNewError::TitleError(message)) => panic!("{}", message),
+        Err(TicketNewError::DescriptionError(message)) => 
+            Ticket::new(title.clone(), "Description not provided".into(), status.clone()).unwrap(),
+        Ok(ticket) => ticket,
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
